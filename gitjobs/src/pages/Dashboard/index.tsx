cdfd40526b94885f9c6196728da1bf13427ac0cc
Lucas Silva <lucasimap@alu.ufc.br>
2020-08-12 15:41:40 -0300

    Adicionando styled-components e criando dasboard

--- gitjobs/src/pages/Dashboard/index.tsx
+++ gitjobs/src/pages/Dashboard/index.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
+import logoImg from '../../assets/githubLogo.png';
+import { FiChevronRight } from 'react-icons/fi';
 
+import {Title, Form, Jobs } from './styles';
 const Dasboard: React.FC = () =>{
-  return <h1>Dashboard</h1>
+  return(
+    <>
+      <img src={logoImg} alt="Github Logo"/>
+      <Title>Encontre Jobs sendo ofertados </Title>
+
+      <Form action="">
+          <input placeholder="Busque por vagas"/>
+          <button type="submit" >Pesquisar</button>
+      </Form>
+
+      <Jobs>
+        <a href="teste">
+          <img
+            src="https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb3VIIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--04344d944b21465d33ecd7bd62098692640a8658/herman%206.jpg"
+            alt="logo"
+          />
+          <div>
+            <strong>Head of Digital UX/UI</strong>
+            <p>Miele X – Full Time</p>
+          </div>
+
+          <FiChevronRight  size={20}/>
+        </a>
+        <a href="teste">
+          <img
+            src="https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb3VIIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--04344d944b21465d33ecd7bd62098692640a8658/herman%206.jpg"
+            alt="logo"
+          />
+          <div>
+            <strong>Head of Digital UX/UI</strong>
+            <p>Miele X – Full Time</p>
+          </div>
+
+          <FiChevronRight  size={20}/>
+        </a>
+        <a href="teste">
+          <img
+            src="https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb3VIIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--04344d944b21465d33ecd7bd62098692640a8658/herman%206.jpg"
+            alt="logo"
+          />
+          <div>
+            <strong>Head of Digital UX/UI</strong>
+            <p>Miele X – Full Time</p>
+          </div>
+
+          <FiChevronRight  size={20}/>
+        </a>
+      </Jobs>
+    </>
+  )
 }
 
 export default Dasboard;
